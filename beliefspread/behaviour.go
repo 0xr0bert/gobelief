511f30049f5e372b032a24b5cb04227a943f715e
Robert Greener <me@r0bert.dev>
2022-12-16 22:11:14 +0000

    Document behaviour

--- beliefspread/behaviour.go
+++ beliefspread/behaviour.go
@@ -4,11 +4,18 @@ import (
 	"github.com/google/uuid"
 )
 
+// A Behaviour in the simulation.
 type Behaviour struct {
+	// The name of the behaviour.
 	Name string
+	// The UUID of the behaviour.
 	Uuid uuid.UUID
 }
 
+// Create a new behaviour.
+// Name is the name of the behaviour.
+//
+// This behaviour will have a randomly generated UUID.
 func NewBehaviour(name string) (b *Behaviour) {
 	b = new(Behaviour)
 	b.Name = name
