b96c23a493d5279538d1a98443021f9d895b6e62
Robert Greener <me@r0bert.dev>
2022-12-20 10:01:16 +0000

    Document runner.go

--- runner/runner.go
+++ runner/runner.go
@@ -11,22 +11,35 @@ import (
 	"go.uber.org/zap"
 )
 
+// The configuration of the simulation.
 type Configuration struct {
+	// The behaviours in the simulation.
 	Behaviours []*b.Behaviour
-	Beliefs    []*b.Belief
-	Agents     []*b.Agent
-	Prs        PerformanceRelationships
-	StartTime  b.SimTime
-	EndTime    b.SimTime
+	// The beliefs in the simulation.
+	Beliefs []*b.Belief
+	// The agents in the simulation.
+	Agents []*b.Agent
+	// The PerformanceRelationships in the simulation.
+	Prs PerformanceRelationships
+	// The start time of the simulation.
+	StartTime b.SimTime
+	// The end time of the simulation (inclusive).
+	EndTime b.SimTime
+	// The output file.
 	OutputFile *os.File
+	// Whether to serialize the full state of agents, or just summary stats.
 	FullOutput bool
 }
 
+// The simulation runner.
 type Runner struct {
+	// The configuration.
 	Configuration *Configuration
-	Logger        *zap.Logger
+	// The logger.
+	Logger *zap.Logger
 }
 
+// Run the simulation.
 func (r *Runner) Run() {
 	r.Logger.Info(
 		"Running simulation",
@@ -53,6 +66,9 @@ func (r *Runner) Run() {
 	}
 }
 
+// Serialize the full state of agents as the output.
+//
+// This is stored as a zstd-compressed JSON file.
 func (r *Runner) serializeFullOutput() error {
 	specs := make([]*AgentSpec, len(r.Configuration.Agents))
 
@@ -92,6 +108,16 @@ func (r *Runner) serializeFullOutput() error {
 	return nil
 }
 
+// Serialize summary statistics about the agents.
+//
+// This calculates:
+// - the number of agents performing each behaviour;
+// - the mean activation for each belief;
+// - the standard deviation of the activation for each belief;
+// - the median activation for each belief; and
+// - the number of agents who have non-zero activation for each belief.
+//
+// This is stored as a zstd-compressed JSON file.
 func (r *Runner) serializeOutput() error {
 	specs := NewOutputSpecs(
 		r.Configuration.Agents,
@@ -127,12 +153,14 @@ func (r *Runner) serializeOutput() error {
 	return nil
 }
 
+// Tick between two times (inclusive).
 func (r *Runner) tickBetween(start, end b.SimTime) {
 	for i := start; i <= end; i++ {
 		r.tick(i)
 	}
 }
 
+// "Tick" the simulation (run it for one time step - time).
 func (r *Runner) tick(time b.SimTime) {
 	r.Logger.Info("Perceiving beliefs", zap.Uint32("Day", uint32(time)))
 	r.perceiveBeliefs(time)
@@ -140,12 +168,25 @@ func (r *Runner) tick(time b.SimTime) {
 	r.performActions(time)
 }
 
+// Perceive the beliefs the agent holds for every agent.
+//
+// This updates all the agent's beliefs for every agent at the specified time
+// step.
 func (r *Runner) perceiveBeliefs(time b.SimTime) {
 	for _, a := range r.Configuration.Agents {
 		a.UpdateActivationForAllBeliefs(time, r.Configuration.Beliefs)
 	}
 }
 
+// Perform an action for a specified agent at a specified time.
+//
+// If the preference for behaviours is fully negative, the "least-bad" option is
+// chosen.
+//
+// If only one is positive, this option is chosen.
+//
+// If more than one is positive, it is chosen probabilistically based upon the
+// preference.s
 func (r *Runner) agentPerformAction(agent *b.Agent, time b.SimTime) {
 	type probPair struct {
 		behaviour *b.Behaviour
@@ -208,6 +249,7 @@ func (r *Runner) agentPerformAction(agent *b.Agent, time b.SimTime) {
 	}
 }
 
+// Perform actions for all agents at the specified time.
 func (r *Runner) performActions(time b.SimTime) {
 	for _, a := range r.Configuration.Agents {
 		r.agentPerformAction(a, time)
